54bc280031adf164daba2a4a5b22a67558834739
jaydipjadhav <jaydipdjadhav>
2024-07-22 12:34:05 +0530

    suceessfullt create and run presigned url

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-console.log("aws with nodejs")
-
-import {  S3Client } from "@aws-sdk/client-s3" 
-
+import {  GetObjectCommand, S3Client } from "@aws-sdk/client-s3" 
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner"
 
 const s3Client = new S3Client({
-    region : "ap-south-1",
+    region : "us-east-1",
     credentials :{
-        accessKeyId :"",
-        
+        accessKeyId : "",
+        secretAccessKey : ""
     }
 })
 
+
+async function getObjectS3(mykey){
+
+    console.log("key is : " , mykey)
+    const command = new GetObjectCommand({
+    Bucket : "private.jaydipdev",
+      Key : mykey
+    })
+
+    const url = await getSignedUrl(s3Client , command);
+    console.log(url);
+
+    return url;
+
+}
+
+
+async function init(){
+    console.log("getObject url :  " , await getObjectS3("jaydip.JPG"));
+
+    console.log("creating successfully getObject PreSigned url")
+}
+
+init();
